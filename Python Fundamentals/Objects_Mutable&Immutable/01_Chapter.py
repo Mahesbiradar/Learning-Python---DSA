ae3cf70eff19f3971b080b2972ebf4fbc7ef8a2d
Mahesbiradar <maheshbiradar103@gmail.com>
2026-04-03 11:32:36 +0530

    Add initial implementations for mutable and immutable objects in Python

---
+++ Python Fundamentals/Objects_Mutable&Immutable/01_Chapter.py
@@ -0,0 +1,4 @@
+Num1=2
+print(F"The ID of num1:{id(Num1)}")  
+Num1=5
+print(F"The ID of num1:{id(Num1)}")  
---
+++ Python Fundamentals/Objects_Mutable&Immutable/02_Chapter.py
@@ -0,0 +1,12 @@
+names=set()
+
+print(F"The contents of the set():{names}")
+print(F"The ID of the set():{id(names)}")
+
+names.add("Mahesh")
+names.add("Rahul")
+print(F"The contents of the set():{names}")
+print(F"The ID of the set():{id(names)}")
+names.add("Suresh")
+print(F"The contents of the set():{names}")
+print(F"The ID of the set():{id(names)}")
