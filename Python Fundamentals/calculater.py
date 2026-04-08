44f6950b5ba09678f4cf8a61b0f0cbbf025cbdcc
Mahesbiradar <maheshbiradar103@gmail.com>
2026-04-08 11:42:02 +0530

    Add initial implementations for calculator and data type examples

---
+++ Python Fundamentals/calculater.py
@@ -0,0 +1,30 @@
+# def add(a,b):
+#     return a+b
+
+# def sub(a,b):
+#     return a-b
+
+# def mul(a,b):
+#     return a*b
+
+# def div(a,b):
+#     return a/b
+
+# def calculator():
+#     num1=int(input("Enter the Num1: "))
+#     oprator=input("Enter the operator: ")
+#     num2=int(input("Enter the Num2: "))
+
+#     if oprator=="+":
+#         print("The Result:",add(num1,num2))
+#     elif oprator=="-":
+#         print("The Result:",sub(num1,num2))
+#     elif oprator=="*":
+#         print("The Result:",mul(num1,num2))
+#     elif oprator=="/":
+#         print("The Result:",div(num1,num2))
+#     else:
+#         print("Invalid Operator")
+# calculator()
+
+
