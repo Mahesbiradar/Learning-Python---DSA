7254978e0996def8d92a53b14aae96c8de4bd55a
mahesbiradar <maheshbiradar103@gmail.com>
2026-09-23 09:59:35 +0530

    Day 68 started with new problem and revision

---
+++ Daily_Work/DAY_68_SOLUTIONS.py
@@ -0,0 +1,56 @@
+# DAY 68
+
+# Status:
+# Time Taken:
+# Time Complexity:
+# Space Complexity:
+# Submitted to LC:
+# Result:
+# Pattern:
+# Variant:
+# Mistakes / Confusion:
+
+
+
+# New 
+"""
+
+1	-	15	-	3Sum	-	Two Pointers	-	Maximize/minimize between ends
+2	-	16	-	3Sum Closest	-	Two Pointers	-	Maximize/minimize between ends
+
+"""
+
+# Tier - 1
+
+"""
+1	-	862	-	Shortest Subarray with Sum at Least K
+2	-	1696	-	Jump Game VI
+3	-	680	-	Valid Palindrome II
+4	-	42	-	Trapping Rain Water
+
+"""
+
+# Tier - 2
+
+"""
+1	-	2262	-	Total Appeal of A String
+
+"""
+
+# Tier - 3
+
+"""
+1	-	560	-	Subarray Sum Equals K
+2	-	525	-	Contiguous Array
+3	-	80	-	Remove Duplicates from Sorted Array II
+
+"""
+
+# Tier - 4
+
+"""
+1	-	3	-	Longest Substring Without Repeating
+2	-	209	-	Minimum Size Subarray Sum
+
+"""
+
